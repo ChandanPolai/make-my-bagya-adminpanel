7fc83b4cc1c636ff6055ef70f77f05168f436af3
ChandanPolai <chandanpolai26@gmail.com>
2025-10-29 18:07:51 +0530

    update url

--- src/env/env.local.ts
+++ src/env/env.local.ts
@@ -1,13 +1,6 @@
-// export const environment = {
-//   production: false,
-//   baseURL: 'http://localhost:7026',
-//   imageUrl: 'http://localhost:7026/',
-//   route: 'admin'
-// };
-
 export const environment = {
   production: false,
-  baseURL: 'makemybhagya.itfuturz.in',
-  imageUrl: 'makemybhagya.itfuturz.in/',
-  route:'admin'
-}
+  baseURL: 'http://localhost:7026',
+  imageUrl: 'http://localhost:7026/',
+  route: 'admin'
+};
