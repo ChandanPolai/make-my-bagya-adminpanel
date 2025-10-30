dad10f5c77809b288a55800f9c0a46454f319de2
ChandanPolai <chandanpolai26@gmail.com>
2025-10-30 18:41:35 +0530

    update

--- src/env/env.local.ts
+++ src/env/env.local.ts
@@ -1,13 +1,13 @@
-// export const environment = {
-//   production: false,
-//   baseURL: 'http://localhost:7026',
-//   imageUrl: 'http://localhost:7026/',
-//   route: 'admin'
-// };
-
 export const environment = {
   production: false,
-  baseURL: 'https://makemybhagya.itfuturz.in',
-  imageUrl: 'https://makemybhagya.itfuturz.in/',
-  route:'admin'
-}
+  baseURL: 'http://localhost:7026',
+  imageUrl: 'http://localhost:7026/',
+  route: 'admin'
+};
+
+// export const environment = {
+//   production: false,
+//   baseURL: 'https://makemybhagya.itfuturz.in',
+//   imageUrl: 'https://makemybhagya.itfuturz.in/',
+//   route:'admin'
+// }
